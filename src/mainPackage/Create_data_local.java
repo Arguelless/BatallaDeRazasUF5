5338acf1b5035caf842fe7fafd2749c32e448399
Y0URGOD <Y0URGOD@DESKTOP-VBMPHNP>
2023-05-05 20:18:34 +0200

    datos para programar sin base datos

--- src/mainPackage/Create_data_local.java
+++ src/mainPackage/Create_data_local.java
@@ -1,6 +1,6 @@
 package mainPackage;
 
-public class Create_data {
+public class Create_data_local {
 
 	public static void main(String[] args) {
 		Weapons pepe_enano = new Weapons(null, null, null) ;
