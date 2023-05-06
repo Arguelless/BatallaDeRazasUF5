164187dbdfed55b917a9fdc182a3bf62e2f1e4e3
Y0URGOD <Y0URGOD@DESKTOP-VBMPHNP>
2023-05-06 17:07:03 +0200

    Add Exption

---
+++ src/mainPackage/Exceptions.java
@@ -0,0 +1,17 @@
+package mainPackage;
+
+public class Exceptions extends Exception{
+	//
+}
+
+class Starting_windows_exception extends Exception{
+	private String mensaje;
+	
+	public Starting_windows_exception(String mensaje) {
+		
+		super(mensaje);
+
+	}
+	
+
+}
--- src/mainPackage/Starting_Window.java
+++ src/mainPackage/Starting_Window.java
@@ -1,7 +1,6 @@
 package mainPackage;
 
 import javax.swing.*;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,9 +10,11 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Starting_Window extends JFrame implements ActionListener{
+	private boolean character_choosed=false;
+	private JLabel error_messages;
 	private BufferedImage image1,icon;
-	private JPanel p_principal,p1,p2,panel_image;
-	private JButton play,ranking,exit;
+	private JPanel p_principal,p1,p2,p3,panel_image;
+	private JButton play,c_character,c_weapon,exit;
     public Starting_Window() {
     	
     	try {
@@ -28,41 +29,78 @@ public class Starting_Window extends JFrame implements ActionListener{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setIconImage(icon);
         
+        error_messages=new JLabel("");
+        error_messages.setForeground(Color.RED);
+        error_messages.setFont(new Font("Arial", Font.BOLD, 20)); 
+        
         p_principal=new JPanel();
         p_principal.setLayout(new BoxLayout(p_principal,BoxLayout.Y_AXIS));
 		p1 = new JPanel();
-		p1.setLayout(new FlowLayout(FlowLayout.CENTER));
+		p1.setLayout(new FlowLayout(FlowLayout.CENTER,20,10));
 		p2 = new JPanel();
-		p2.setLayout(new FlowLayout(FlowLayout.CENTER));
+		p2.setLayout(new FlowLayout(FlowLayout.CENTER,20,10));
+		p3 = new JPanel();
+		p3.setLayout(new FlowLayout(FlowLayout.CENTER,20,10));
 		p_principal.add(p1);
 		p_principal.add(p2);
+		p_principal.add(p3);
 		add(p_principal);
     	
 		panel_image = new Image();
 		p1.add(panel_image);
 		
 		play=new JButton("Play");
-		ranking=new JButton("Ranking");
+		c_character=new JButton("Choose a Character");
+		c_weapon=new JButton("Choose a Weapon");
 		exit=new JButton("Exit");
 		
-		p2.add(play);
-		p2.add(ranking);
-		p2.add(exit);
 		
+		p2.add(play);
+		p2.add(c_character);
+		p2.add(c_weapon);
+		p2.add(exit);
         
+		p3.add(error_messages);
         
-        
-        
+
+		play.addActionListener(this);
+		c_character.addActionListener(this);
+		c_weapon.addActionListener(this);
+		exit.addActionListener(this);
 
         setVisible(true);
     }
 
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+public void actionPerformed(ActionEvent evento) {
+    	
+        try {
+        	
+        	
+            if (evento.getSource() == play) {
+                if(character_choosed==false) {
+                	throw new Starting_windows_exception("Error: You can't play without any characters selected");
+                } else {
+                	error_messages.setVisible(false);
+                }
+            } else if (evento.getSource() == c_character) {
+            	System.out.println("Queda para hacer");
+            	character_choosed=true;
+            	error_messages.setVisible(false);
+            } else if (evento.getSource() == c_weapon) {
+            	if(character_choosed==false) {
+                	throw new Starting_windows_exception("Error: You can't choose a weapon without any characters selected");
+                } else {
+                	error_messages.setVisible(false);
+                }
+            } 
+            else if (evento.getSource() == exit) {
+            	dispose();
+            }
+        } catch (Starting_windows_exception e) {
+        	error_messages.setText(e.getMessage());
+        	error_messages.setVisible(true);
+        } 
+    }
 
     
 }
