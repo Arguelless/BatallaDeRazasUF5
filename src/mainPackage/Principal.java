1b40a8dd6305204bdc8e2e37fa117ad6a23c57c9
chicaalexgitthub <chica_alex_gitthub@gmx.es>
2023-05-08 16:28:24 +0200

    probando la base de datos

--- src/mainPackage/Principal.java
+++ src/mainPackage/Principal.java
@@ -1,9 +1,24 @@
 package mainPackage;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Principal {
+	private BufferedImage img;
 	public Principal() {
 		super();
+		try {
+			img = ImageIO.read(new File("./src/Images/Daga.jpg"));
+			System.out.println(img.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	public static void main(String[] args) {
+		new Principal();
 	}
 }
