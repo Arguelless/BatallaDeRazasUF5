639304645c53642b05b1a9e59e890a69769891ef
chicaalexgitthub <chica_alex_gitthub@gmx.es>
2023-05-18 18:22:07 +0200

    organized the folders

---
+++ M02/database.sql
@@ -0,0 +1,58 @@
+create database if not exists BDDbatalla;
+
+use BDDbatalla;
+
+create table if not exists weapons(
+	weapon_id			int				PRIMARY KEY AUTO_INCREMENT,
+    weapon_name			varchar(100)	NOT NULL,
+    weapon_image_path	varchar(100) 	NOT NULL,
+    weapon_power		int				NOT NULL,
+    weapon_speed		int				NOT NULL
+);
+
+create table if not exists races(
+	race_id				int				PRIMARY KEY AUTO_INCREMENT,
+    race_name			VARCHAR(100)	NOT NULL,
+    health				int 			NOT NULL,
+    power				int 			NOT NULL,
+    defense				int 			NOT NULL,
+    agility				int 			NOT NULL,
+    speed				int 			NOT NULL
+);
+
+create table if not exists warriors(
+	warrior_id			int				PRIMARY KEY AUTO_INCREMENT,
+    warrior_name		varchar(100)	NOT NULL,
+    warrior_image_path	varchar(100) 	NOT NULL,
+    warrior_race		int				NOT NULL,
+    FOREIGN KEY (warrior_race)	REFERENCES	races		(race_id)
+);
+
+create table if not exists players(
+	player_id			int				PRIMARY KEY AUTO_INCREMENT,
+    player_name			varchar(100)	NOT NULL
+);
+
+create table if not exists weapons_available(
+	weapon_id			int,
+    warrior_id			int,
+    FOREIGN KEY	(warrior_id)	REFERENCES	warriors	(warrior_id),
+    FOREIGN KEY	(weapon_id)		REFERENCES	weapons		(weapon_id)
+);
+
+create table if not exists battle(
+	battle_id			int				PRIMARY KEY AUTO_INCREMENT,
+    player_id			int,
+    warrior_id			int,
+    warrior_weapon_id	int,
+    opponent_id			int,
+    opponent_weapon_id 	int,
+    injuries_caused		int,
+    injuries_suffered	int,
+    battle_points		int,
+    FOREIGN KEY	(warrior_weapon_id)		REFERENCES	weapons		(weapon_id),
+    FOREIGN KEY	(warrior_id)			REFERENCES	warriors	(warrior_id),
+    FOREIGN KEY	(player_id)				REFERENCES	players		(player_id),
+    FOREIGN KEY	(opponent_id)			REFERENCES	players		(player_id),
+    FOREIGN KEY	(opponent_weapon_id)	REFERENCES	weapons		(weapon_id)
+);
